fb5211c5f1eb6545d47b8b760d05d7a1c857aa6a
rookiejj9 <rookiejj9@gmail.com>
2024-07-01 22:01:55 +0800

    步骤25

--- app.py
+++ app.py
@@ -8,17 +8,21 @@ app.run(debug=True)
 def practice1_html():  # put application's code here
     return render_template('practise1/practise1.html')
 
+
 @app.route('/practise2')
 def practice2_html():  # put application's code here
     return render_template('practise2/practise2.html')
+
 
 @app.route('/practise3')
 def practice3_html():  # put application's code here
     return render_template('practise3/practise3.html')
 
+
 @app.route('/practise4')
 def practice4_html():
     return render_template('practise4/practise4.html')
+
 
 if __name__ == '__main__':
     app.run()
