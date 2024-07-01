164937008f17f8d7560d90a59b26fe6cdbeb80f7
rookiejj9 <rookiejj9@gmail.com>
2024-07-01 22:06:33 +0800

    对练习备注

--- app.py
+++ app.py
@@ -4,21 +4,22 @@ app = Flask(__name__)
 app.run(debug=True)
 
 
+# 调查问卷项目
 @app.route('/practise1')
 def practice1_html():  # put application's code here
     return render_template('practise1/practise1.html')
 
-
+# 盒子模型
 @app.route('/practise2')
 def practice2_html():  # put application's code here
     return render_template('practise2/practise2.html')
 
-
+# 弹性盒模型
 @app.route('/practise3')
 def practice3_html():  # put application's code here
     return render_template('practise3/practise3.html')
 
-
+# 学习排版
 @app.route('/practise4')
 def practice4_html():
     return render_template('practise4/practise4.html')
